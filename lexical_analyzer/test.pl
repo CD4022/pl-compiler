48e9828a0eb9ffa3c7cbc1d4ad49ac5195ab5955
Alireza Karimi <alireza.mathers@gmail.com>
2024-03-25 17:30:22 +0330

    add the test program

---
+++ lexical_analyzer/test.pl
@@ -0,0 +1,19 @@
+int _main ( )
+{
+    int a ;
+    a = 1 ;
+    int _i , _inc , _j ;
+    _i = 0 ;
+    _inc = 2 ;
+
+    if ( _i < _j )
+    {
+        _i = _i + _inc ;
+        a = a * _i ;
+    }
+
+    if ( a > 10 )
+    {
+        print ( a + _i ) ;
+    }
+}
