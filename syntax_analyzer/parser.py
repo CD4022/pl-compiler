72a4cd7b931fce34c4f710b98b6d88a80ae059f6
Alireza Karimi <alireza.mathers@gmail.com>
2024-05-16 14:25:13 +0330

    Parser init

---
+++ syntax_analyzer/parser.py
@@ -0,0 +1,16 @@
+from sys import argv
+from lexical_analyzer import lexer
+
+
+class Grammar:
+    def __init__(self, rules=None):
+        self.rules = []
+        if rules:
+            self.rules = self.rules.extend(rules)
+
+
+class Rule:
+    def __init__(self, name, body=None):
+        self.name = name
+        
+
