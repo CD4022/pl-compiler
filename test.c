c3b08c841dc133d8690ba55db127a8a3608ab8b0
Ali Pourgheysari <a.geysar@gmail.com>
2024-06-11 16:34:58 +0330

    Fix else if error

--- test.c
+++ test.c
@@ -1,1 +1,33 @@
-int a, b;
+void print_s(char arr,int a,int b)
+{
+    for (a = "start";){
+        break;
+    }
+    b = a;
+    a = ((a * 10) + -1);
+    b = 0x3f;
+    if (!b){
+        return 0;
+    }
+    else if (b){
+        if (a) {
+            print(a);
+        }
+        else{
+            print("goox");
+        }
+    }
+
+    else if(b + a == 10 && c + b == (-0x34)){
+        print("moox");
+    }
+    b = +10;
+    b = -a;
+    print(arr);
+}
+
+int main(){
+    char arr[5] = "alireza khar";
+    print_s(arr, a, b);
+    return -0;
+}
