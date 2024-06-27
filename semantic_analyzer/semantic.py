cfdb0320f698af4408e64fd76ae114764db82913
Ali Pourgheysari <a.geysar@gmail.com>
2024-06-28 00:43:00 +0330

    Add binary expression validation

--- semantic_analyzer/semantic.py
+++ semantic_analyzer/semantic.py
@@ -1,5 +1,5 @@
 from sys import argv
-import json
+import constants
 
 from syntax_analyzer import parser
 
@@ -34,27 +34,54 @@ def traverse_declaration(node: parser.Node):
         pass # TODO
 
 
+def traverse_bin_expr(node: parser.Node):
+
+    if node.children[1].children[0].children[0].value in constants.bin_operators:
+        # Check the first child
+        is_valid = check_child(node)
+        # Check the second child
+        if is_valid:
+            check_child(node.children[1].children[1])
+
+
+def check_child(node):
+    while len(node.children) != 0:
+        node = node.children[0]
+
+        if node.value == 'expr':
+            traverse_bin_expr(node)
+
+        if str(node.value).startswith('T_'):
+            if node.value in constants.non_int_terminals:
+                print("operands do not have the same type")
+                return False
+            elif node.value in constants.int_terminals:
+                return True
+            node = node.parent.children[1]
+
 
 def traverse_parse_tree(node: parser.Node, depth=0):
     current = node
     for child in node.children:
-        if child.value == "declaration":
-            traverse_declaration(child)
+        # if child.value == "declaration":
+        #     pass # TODO: Alireza
         if child.value == "expr":
-            pass # TODO: Ali
-        if child.value == "if":
-            pass # TODO: Ali
-        if child.value == "func_call":
-            pass # TODO: Alireza
-        if child.value == "main":
-            pass # TODO: Alireza
-        if child.value == "args":
-            pass # TODO: Alireza
-        if child.value == "pars":
-            pass # TODO: Ali
-        if child.value == "return":
-            pass # TODO: Alireza
+            traverse_bin_expr(child)
+        # elif child.value == "if":
+        #     pass # TODO: Ali
+        # elif child.value == "func_call":
+        #     pass # TODO: Alireza
+        # elif child.value == "main":
+        #     pass # TODO: Alireza
+        # elif child.value == "args":
+        #     pass # TODO: Alireza
+        # elif child.value == "pars":
+        #     pass # TODO: Ali
+        # elif child.value == "return":
+        #     pass # TODO: Alireza
 
+        elif len(child.children) != 0:
+            traverse_parse_tree(child)
 
 
 def semantic_analyze(parse_tree: parser.Node):
@@ -65,7 +92,7 @@ def semantic_analyze(parse_tree: parser.Node):
 def main():
     parse_tree = parser.parse(argv[1])
     if parse_tree:
-        pass
+        semantic_analyze(parse_tree)
 
 
 if __name__ == "__main__":
