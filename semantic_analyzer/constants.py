0b375df8f97d44e74b0242a741b07c4ee0072108
Ali Pourgheysari <a.geysar@gmail.com>
2024-06-28 00:41:26 +0330

    Create constants.py

---
+++ semantic_analyzer/constants.py
@@ -0,0 +1,27 @@
+bin_operators = [
+    'T_PLUS',
+    'T_MINUS',
+    'T_MULT',
+    'T_DIV',
+    'T_MOD',
+    'T_EQUALS',
+    'T_NOT-EQUALS',
+    'T_GT',
+    'T_LT',
+    'T_GE',
+    'T_LE',
+    'T_ASSIGN'
+]
+
+int_terminals = [
+    "T_DEC_VAL",
+    "T_HEX_VAL"
+]
+
+non_int_terminals = [
+    'T_STR_VAL',
+    'T_CHAR_VAL',
+    'T_ID',
+    'T_TRUE',
+    'T_FALSE'
+]
