0caf65345f66d77a7fb985fcccee9148b3c901d4
Alireza Karimi <alireza.mathers@gmail.com>
2024-06-30 20:47:33 +0330

    Error found

--- test.c
+++ test.c
@@ -1,33 +1,7 @@
-void print_s(char arr,int a,int b)
-{
-    for (a = "start";;){
-        break;
-    }
-    b = a;
-    a = ((a * 10) + -1);
-    b = 0x3f;
-    if (!b){
-        return 0;
-    }
-    else if (b){
-        if (a) {
-            print(a);
-        }
-        else{
-            print("goox");
-        }
-    }
-
-    else if(b + a == 10 && c + b == (-0x34)){
-        print("moox");
-    }
-    b = +10;
-    b = -a;
-    print(arr);
-}
-
 int main(){
-    char arr[5] = "ali khar";
-    print_s(arr, a, b);
-    return -0;
+    int a;
+    int b;
+    a = b;
+    return 'b'; // ISSUE: type of 'b' is char!!! returns INVALID and produces an error
 }
+
